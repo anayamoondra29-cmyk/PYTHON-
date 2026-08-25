c489bd65eb31b4391446168ce2a08fc41eda0e83
anayamoondra29-cmyk <anayamoondra29@gmail.com>
2026-08-25 19:22:55 +0530

    Add files via upload

---
+++ percentage.py
@@ -0,0 +1,7 @@
+english=int(input("enter english marks"))
+math=int(input("enter math marks"))
+science=int(input("enter science marks"))
+hindi=int(input("enter hindi marks"))
+sum=english+math+science+hindi
+percentage=(sum/400)*100
+print("precentage",percentage)
