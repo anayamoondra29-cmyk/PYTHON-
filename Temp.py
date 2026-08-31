341adcbe20bcf2d79219637c7f1dc5f6519bb1ba
anayamoondra29-cmyk <anayamoondra29@gmail.com>
2026-08-31 15:22:19 +0530

    Add files via upload

---
+++ Temp.py
@@ -0,0 +1,5 @@
+temperature = int(input("Enter today's temperature"))
+if temperature >= 35:
+    print("It's scorching! Time to go swimming or stay in the AC.")
+elif temperature >= 25: 
+     print("Perfect summer weather! Let's go for a picnic in the park.")
