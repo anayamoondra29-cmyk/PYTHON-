1a3a534b2861922c24cee6c5e040ba197f805cfd
anayamoondra29-cmyk <anayamoondra29@gmail.com>
2026-09-01 19:02:48 +0530

    Add files via upload

---
+++ BMI.py
@@ -0,0 +1,19 @@
+hight = float(input("enter your hight"))
+weight = float(input("enter your weight"))
+
+BMI = weight / (hight/100)**2
+
+print("your BMI is", BMI)
+
+if BMI<= 18.4:
+    print("you are underweight")
+elif BMI <= 24.9:
+    print("you are healthy")
+elif BMI <= 29.9:
+    print("you are over weight")
+elif BMI<= 34.9:
+    print("you are severly overweight")
+elif BMI<= 39.9:
+    print("you are obese")
+else:
+    print("you are severly overweight")
