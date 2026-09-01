71d813b2324b46472c416acb5df9768adf8ea3cc
anayamoondra29-cmyk <anayamoondra29@gmail.com>
2026-09-01 19:17:15 +0530

    Add files via upload

---
+++ And or oprator.py
@@ -0,0 +1,7 @@
+A=int(input("enter a number"))
+B=int(input("enter a number"))
+
+if A>0 and B>0:
+    print("both numbers are postive")
+elif A>0 or B>0:
+    print("one of the numbers are postive")
